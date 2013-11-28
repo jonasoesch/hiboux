7b6b4a48e64b8783ac5bdc9a4e79be464d752774
Yoann Gern <webyg@icloud.com>
2013-11-28 14:52:34 +0100

    add Registration entities to Core Data

---
+++ Registration.h
@@ -0,0 +1,33 @@
+//
+//  Registration.h
+//  hiboux
+//
+//  Created by Yoann Gern on 28.11.13.
+//  Copyright (c) 2013 Jonas Oesch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Registration : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * coorX;
+@property (nonatomic, retain) NSNumber * coorY;
+@property (nonatomic, retain) NSDate * timestamp;
+@property (nonatomic, retain) NSNumber * altitude;
+@property (nonatomic, retain) NSNumber * age;
+@property (nonatomic, retain) NSString * sexe;
+@property (nonatomic, retain) NSNumber * no_ring;
+@property (nonatomic, retain) NSNumber * weight;
+@property (nonatomic, retain) NSNumber * wing_size;
+@property (nonatomic, retain) NSNumber * tarse;
+@property (nonatomic, retain) NSString * comments;
+@property (nonatomic, retain) NSString * spieces;
+@property (nonatomic, retain) NSString * family;
+@property (nonatomic, retain) NSString * locationName;
+@property (nonatomic, retain) NSString * statusWeather;
+@property (nonatomic, retain) NSNumber * temperature;
+@property (nonatomic, retain) NSString * classe;
+
+@end
---
+++ Registration.m
@@ -0,0 +1,32 @@
+//
+//  Registration.m
+//  hiboux
+//
+//  Created by Yoann Gern on 28.11.13.
+//  Copyright (c) 2013 Jonas Oesch. All rights reserved.
+//
+
+#import "Registration.h"
+
+
+@implementation Registration
+
+@dynamic coorX;
+@dynamic coorY;
+@dynamic timestamp;
+@dynamic altitude;
+@dynamic age;
+@dynamic sexe;
+@dynamic no_ring;
+@dynamic weight;
+@dynamic wing_size;
+@dynamic tarse;
+@dynamic comments;
+@dynamic spieces;
+@dynamic family;
+@dynamic locationName;
+@dynamic statusWeather;
+@dynamic temperature;
+@dynamic classe;
+
+@end
