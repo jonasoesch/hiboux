bb948d5fe96419969a4a4465208adf8b05fe674b
Yoann Gern <webyg@icloud.com>
2014-01-15 19:30:57 +0100

    Add dynamically Species, Family and Class

--- hiboux/OWLSelectionViewController.m
+++ hiboux/OWLSelectionViewController.m
@@ -76,6 +76,15 @@
 {
     OWLAddOwlViewController *addView = (OWLAddOwlViewController *) segue.destinationViewController;
     
+    NSArray *speciesInfo = [OWLHelpers speciesInfo];
+    
+    for (NSArray *species in speciesInfo) {
+        if([[species valueForKey:@"Species" ] isEqualToString:[[(UITableViewCell *)sender textLabel] text]]) {
+            [addView.currentRegistration setValue:[species valueForKey:@"Family" ] forKey:@"family"];
+            [addView.currentRegistration setValue:[species valueForKey:@"Class" ] forKey:@"classe"];
+        }
+    }
+    
     [addView.currentRegistration setValue:[[(UITableViewCell *)sender textLabel] text] forKey:@"species"];
     // Pass the selected object to the new view controller.
 }
